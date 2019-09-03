14e15728d82454da6e86869993dbd4b2652ac1e0
Eric McCullough <eric96@live.missouristate.edu>
2019-09-03 11:49:23 -0500

    Implement more db functionality

--- observer/routes/ObserverWebSocketProtocol.py
+++ observer/routes/ObserverWebSocketProtocol.py
@@ -2,13 +2,23 @@ from autobahn.twisted.websocket import WebSocketServerProtocol
 from os import getcwd
 from tinydb import TinyDB, Query
 from tinydb.operations import increment
-import ujson
+import ujson, time
 
 class ObserverWebSocketProtocol(WebSocketServerProtocol):
     def __init__(self):
         super().__init__()
         self._db = TinyDB(getcwd() + '/data/data.json')
         self._dbq = Query()
+
+        self._time_to_update = time.time() + 15
+        self._epochs_since_commit = 0
+
+        self._current_frame = {'1': None,
+                               '2': None,
+                               '3': None,
+                               '4': None
+                               } 
+        self._current_window = {} 
 
     def onConnect(self, request):
         print("Incoming connection from: {}".format(request.peer))
@@ -25,10 +35,30 @@ class ObserverWebSocketProtocol(WebSocketServerProtocol):
 
     def _process_msg(self, msg):
         field, ip  = msg['payload'], msg['ip']
-        if self._db.contains(self._dbq.ip == ip):
-            self._db.update(increment(field), self._dbq.ip == msg['ip'])
-        elif field == 'in':
-            self._db.insert({'ip': ip, 'in': 1, 'out': 0})
+       
+        if ip in self._current_window.keys():
+            self._current_window[ip][field] += 1 
         else:
-            self._db.insert({'ip': ip, 'in': 0, 'out': 1})
+            self._current_window[ip] = {'in': 0, 'out': 0}
+            self._current_window[ip][field] += 1 
+
+        if time.time() > self._time_to_update:
+            self._epochs_since_commit += 1 
+            self._current_frame[str(self._epochs_since_commit)] = self._current_window
+            self._current_window = {} 
+
+            if self._epochs_since_commit >= 4:
+                self._db.insert(self._current_frame) 
+                self._current_frame = {'1': None,
+                                       '2': None,
+                                       '3': None,
+                                       '4': None
+                }
+                                        
+
+    def _reset_time(self):
+        self._time_to_update = time.time() + 15
+
+    def _commit(self):
+        self._db.insert(self._current_frame)
             
